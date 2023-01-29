ab2b4bf355d49bd8b3f9a916af8e87bcd56a4959
Eva Liao <evaliao@EvadeMacBook-Pro.local>
2023-01-29 16:19:45 +0800

    initialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Styled from 'styled-components';
+import Navbar from './compoonents/nav';
+import Background from './img/background.png';
+
+
+const BackGroundImg = Styled.img`
+  
+
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <BackGroundImg src={Background} />
+      <Navbar/>
     </div>
   );
 }
---
+++ src/compoonents/nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const StyledNavbar = Styled.div`
+    background-color:#faf1d1;
+    color:gray;
+    height: 30px;
+    opacity:100%
+`
+
+const Navbar = () => {
+    return (
+        <StyledNavbar>
+            ddd
+            </StyledNavbar>
+    )
+}
+
+export default Navbar;
